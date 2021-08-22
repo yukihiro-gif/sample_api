45f9417c93bcfb812eec1723c4cd8853da4e6446
yukihiro-gif <bsyu14357@gmail.com>
2021-08-22 14:40:45 +0000

    [add]

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class HomesControllerTest < ActionDispatch::IntegrationTest
+class ImagesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
