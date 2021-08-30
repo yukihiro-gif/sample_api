b5f99edf3982418a2e2dc3cf5c8acd9776305468
yukihiro-gif <bsyu14357@gmail.com>
2021-08-30 12:47:21 +0000

    [Tag]add/model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+  has_many :tag_relationships, dependent: :destroy
+  has_many :tag_articles, through: :tag_relationships
 end
--- app/models/tag_article.rb
+++ app/models/tag_article.rb
@@ -1,2 +1,4 @@
 class TagArticle < ApplicationRecord
+  has_many :tab_relationships, dependent: :destroy
+  has_many :articles, through: :tag_relationships
 end
